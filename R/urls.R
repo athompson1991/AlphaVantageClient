6b706637d6e096b23fe267cb1d6bd5f750f67d59
athompson1991 <athompson.git@gmail.com>
2017-08-13 15:25:35 -0700

    Changed file name.

--- R/urls.R
+++ R/urls.R
@@ -3,12 +3,16 @@
 #' 
 #' @param key The API key from AlphaVantage
 #' @export
-setAPIKey <- function(key){
-  api_key <<- key
+setAPIKey <- function(key="demo"){
+  e <<- as.environment("package:AlphaVantageClient")
+  e$api_key <- key
+  e$base_url <- "https://www.alphavantage.co/query"
 }
 
+
+
 buildURL <- function(named_list){
-  outURL <- paste0("https://www.alphavantage.co/query?")
+  outURL <- paste0(e$base_url, "?")
   for(i in 1:length(named_list)){
     k <- names(named_list)[i]
     v <- named_list[i]
@@ -26,10 +30,12 @@ buildURLTimeSeries <- function(timeType, symbol, interval, outputsize="full", da
   
   params_list <- list(
     "function" = paste0("TIME_SERIES_", toupper(timeType))
-    ,"symbol" = symbol
+    ,"symbol" = toupper(symbol)
     ,"interval" = interval
-    ,"apikey" = api_key
+    ,"datatype" = datatype
+    ,"apikey" = e$api_key
   )
+  
   url <- buildURL(params_list)
   return(url)
 }
--- tests/testthat/test-urls.R
+++ tests/testthat/test-urls.R
@@ -1,13 +1,14 @@
 context("URL")
 
 test_that("Build URL works as expected", {
+  setAPIKey("ABC3")
   params_list <- list("function" = "time_series", "symbol" = "MSFT", "apikey" = "ABC2")
   expect_equal(buildURL(params_list), "https://www.alphavantage.co/query?function=time_series&symbol=MSFT&apikey=ABC2")
 })
 
 test_that("Build time series URL works as expected", {
   setAPIKey("ABC2")
-  desired_url <- "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=ABC2"
+  desired_url <- "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&datatype=csv&apikey=ABC2"
   url <- buildURLTimeSeries(timeType = "intraday", symbol = "MSFT", interval = "5min")
   expect_equal(url, desired_url)
 })
