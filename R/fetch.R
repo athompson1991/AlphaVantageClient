145823904d28f233de61f94d83469e6279d65aad
athompson1991 <athompson.git@gmail.com>
2017-08-15 11:19:57 -0700

    Added some documentation.

--- R/fetch.R
+++ R/fetch.R
@@ -1,3 +1,15 @@
+#' Fetch time series data
+#' 
+#' This function allows the user to pull data of the five different time series types offered from Alpha Vantage: Intraday, Daily, Daily (Adj. Close),
+#' Weekly, and Monthly. 
+#' 
+#' @param timeType This parameter specifies the time series type. Valid character string input: \code{intraday}, \code{daily}, \code{daily_adjusted}, \code{weekly}, \code{monthly}.
+#' @param symbol Stock symbol, such as MSFT for Microsoft.
+#' @param interval Required argument for intraday trading. Valid character string input: \code{1min}, \code{5min}, \code{15min}, \code{60min}.
+#' @param outputsize Either \code{compact} or \code{full}. Returns only 100 data points versus full-length time series.
+#' @param datatype Either \code{csv} or \code{json}.
+#' @return An \code{xts} object with the time series data.
+#' @export
 fetchTimeSeries <- function(timeType = "intraday", symbol = "spy", interval = "5min", outputsize="full", datatype="csv"){
   out = NULL
   url <- buildURLTimeSeries(timeType = timeType, symbol = symbol, interval = interval, outputsize = outputsize, datatype = datatype)
