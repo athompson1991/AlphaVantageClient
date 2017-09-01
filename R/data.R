a88697a8c126eca2847e955137bbaf0c349a9e2e
athompson1991 <athompson.git@gmail.com>
2017-09-01 10:59:38 -0700

    Cleaned up for CRAN submission

---
+++ R/data.R
@@ -0,0 +1,9 @@
+#' Required and optional arguments specification.
+#'
+#' A listing that replicates the required and optional argument listings from the Alpha Vantage documentation.
+"technical_indicator_specification"
+
+#' Validation listings 
+#'
+#' A listing that outlines which arguments are acceptable.
+"validation_list"
